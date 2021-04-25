c7d862549d1c028fcef8fcb9c1d181a2c718a7a7
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2021-04-25 14:05:32 +0700

    update

---
+++ jajal/baca gambar.py
@@ -0,0 +1,9 @@
+import cv2
+
+# inimebuka gambar
+img = cv2.imread('tes.jpg')
+cv2.imshow('fotone didik', img)
+cv2.waitKey(0)
+cv2.destroyAllWindow()
+
+
---
+++ jajal/dilasi.py
@@ -0,0 +1,6 @@
+nama = 'didik'
+
+if nama == 'didik':
+    print("benar")
+
+    print('salah')
